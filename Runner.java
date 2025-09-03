6eaae6fb5a2e8020c89a4ce83d5eae95cc1f3e9d
sindhujagathees <155611392+sindhujagathees@users.noreply.github.com>
2025-09-03 19:30:09 +0530

    Add files via upload

---
+++ Runner.java
@@ -0,0 +1,7 @@
+package runner;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+@CucumberOptions(features="src\\main\\java\\features\\Login.feature",glue="steps",monochrome=true,publish=true)
+public class Runner extends AbstractTestNGCucumberTests {
+
+}
